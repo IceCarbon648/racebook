a78f2453a6e995f5d4ae285ec65956b5f3185d3b
Vivek Rajaram <vivekrajaram@kcpivot.co.za>
2026-06-17 15:38:12 +0200

    test: registration tests

--- backend/racebookApiTests/UserServiceTest.cs
+++ backend/racebookApiTests/UserServiceTest.cs
@@ -1,29 +1,52 @@
-﻿namespace racebookApiTests;
+﻿using racebookApi.Repositories.Interfaces;
+using NSubstitute;
+using racebookApi.Services;
+using racebookApi.Models.DTOs.FromClient;
+
+namespace racebookApiTests;
 
 public class UserServiceTest
 {
+    private IUserRepository _userRepository;
+
+    RegisterUserDto dto = new RegisterUserDto
+    {
+        Email = "pat@bonana.com",
+        Username = "username",
+        Password = "12345678"
+    };
+
     [SetUp]
     public void Setup()
     {
+        _userRepository = Substitute.For<IUserRepository>();
     }
 
     [Test]
-    public void GivenAnExistingEmail_WhenRegistering_ReturnFalse()
+    public async Task GivenAnExistingEmail_WhenRegistering_ReturnFalse()
     {
         //Arrange
+        _userRepository.UserExists(dto.Email).Returns(true);
+        UserService userService = new UserService(_userRepository);
 
         //Act
+        bool actualResult = await userService.RegisterUserAsync(dto);
 
         //Assert
+        Assert.That(actualResult, Is.False);
     }
 
     [Test]
-    public void GivenAnNonExistentEmail_WhenRegistering_ReturnTrue()
+    public async Task GivenAnNonExistentEmail_WhenRegistering_ReturnTrue()
     {
         //Arrange
+        _userRepository.UserExists(dto.Email).Returns(false);
+        UserService userService = new UserService(_userRepository);
 
         //Act
+        bool actualResult = await userService.RegisterUserAsync(dto);
 
         //Assert
+        Assert.That(actualResult, Is.True);
     }
 }
