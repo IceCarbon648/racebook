bf9a20d9655773b008b7885859eb13fd164dfffd
Vivek Rajaram <vivekrajaram@kcpivot.co.za>
2026-07-17 09:33:28 +0200

    feat: added favourites service layer

---
+++ backend/Business/Interfaces/IFavouriteModService.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Models.DTOs.Response;
+
+namespace Business.Interfaces
+{
+    public interface IFavouriteModService
+    {
+        Task AddToFavourites(string uid, string modId);
+        Task<List<GetModDto>> GetFavourites(string uid);
+    }
+}
