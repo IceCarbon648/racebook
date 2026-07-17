b0092541dafea428104956f5618cc150e59c97b1
Vivek Rajaram <vivekrajaram@kcpivot.co.za>
2026-07-17 10:07:54 +0200

    feat: added favrourites presentation layer

---
+++ backend/racebookApi/Controllers/FavouriteModController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Models.DTOs.Request;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace racebookApi.Controllers
+{
+    public class FavouriteModController : Controller
+    {
+        private readonly IFavouriteModRepository _favouriteModRepository;
+
+        public FavouriteModController(IFavouriteModRepository favouriteModRepository)
+        {
+            _favouriteModRepository = favouriteModRepository;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddToFavourites([FromBody] string modId)
+        {
+            string? uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value.ToString();
+
+            await _favouriteModRepository.AddToFavourites(uid, modId);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetFavourites()
+        {
+            string? uid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value.ToString();
+
+            return Ok(await _favouriteModRepository.GetFavourites(uid));
+        }
+    }
+}
